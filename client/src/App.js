ad948c8c5b90eed487b8f5d82d6124815c7682b2
Marcin Zaremski <marcinzara@gmail.com>
2017-12-12 20:45:00 +0100

    Change a tags to <Link>

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { Container, Menu} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
@@ -14,8 +14,8 @@ class App extends React.Component {
             <Container fluid>
                 <Menu style={{"marginBottom": 0,position: "relative", zIndex: 1000000}}>
                     <Menu.Item header>Historical places map</Menu.Item>
-                    <Menu.Item href="/" name='Home'/>
-                    <Menu.Item href="/addplace" name='Dodaj Miejsce'/>
+                    <Menu.Item as={Link} to='/' name='Home'/>
+                    <Menu.Item as={Link} to='/addplace' name='Dodaj Miejsce'/>
                 </Menu>
 
                 <Route exact path="/" render={() => <Home appMode="showMarkers"/>} />
